306744acb637f34d2032d3352a65dc41ace5aa4e
tiagoaraujo00 <tiagodevjs@gmail.com>
2024-05-16 22:29:25 -0300

    feat: fontes do projeto adicionadas

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,6 +5,10 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amberAccent,
+      ),
+    );
   }
 }
